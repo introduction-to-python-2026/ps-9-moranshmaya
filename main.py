49df058d03f60aa12c569f506c7b71b9c090ac8a
moranshmaya <moranshmaya@gmail.com>
2025-12-30 16:45:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 import lab_setup_do_not_edit
 
 import pandas as pd
@@ -48,5 +48,6 @@ y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 
 accuracy
- 
+import joblib
+joblib.dump(model, "my_model.joblib")
            
